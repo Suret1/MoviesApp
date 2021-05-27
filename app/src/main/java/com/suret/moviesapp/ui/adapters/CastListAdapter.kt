71718cbd6186cf49e7aa277e16f8307ba2a161e5
suret <suretramazanov@gmail.com>
2021-05-27 19:06:26 +0400

    cast list layout add

---
+++ app/src/main/java/com/suret/moviesapp/ui/adapters/CastListAdapter.kt
@@ -0,0 +1,29 @@
+package com.suret.moviesapp.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.suret.moviesapp.databinding.CastListLayoutBinding
+
+class CastListAdapter : RecyclerView.Adapter<CastListAdapter.CastViewHolder>() {
+
+
+    inner class CastViewHolder(private val castListLayoutBinding: CastListLayoutBinding) :
+        RecyclerView.ViewHolder(castListLayoutBinding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CastViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val castListLayoutBinding = CastListLayoutBinding.inflate(layoutInflater)
+        return CastViewHolder(castListLayoutBinding)
+    }
+
+    override fun onBindViewHolder(holder: CastViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
